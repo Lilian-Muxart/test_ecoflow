b90341d52360fc3d50c7222591f9229893182eb9
MUXART <lilian.muxart@gmail.com>
2024-12-10 16:57:13 +0100

    v6

--- custom_components/ecoflow_cloud/devices/public/generator_dual_fuel.py
+++ custom_components/ecoflow_cloud/devices/public/generator_dual_fuel.py
@@ -5,6 +5,7 @@ from ...sensor import StatusSensorEntity, InWattsSolarSensorEntity, DecivoltSens
     DeciampSensorEntity, CelsiusSensorEntity, DecicelsiusSensorEntity, MiscSensorEntity, LevelSensorEntity, DeciwattsSensorEntity, \
     AmpSensorEntity, RemainSensorEntity, DecihertzSensorEntity, WattsSensorEntity, MilliVoltSensorEntity, TempSensorEntity, MiscBinarySensorEntity,  VoltSensorEntity
 from custom_components.ecoflow_cloud.switch import EnabledEntity
+from ...number import ChargingPowerEntity
 
 class Generator_DualFuel(BaseDevice):
     def sensors(self, client: EcoflowApiClient) -> list[BaseSensorEntity]:
@@ -14,12 +15,14 @@ class Generator_DualFuel(BaseDevice):
             RemainSensorEntity(client, self, "pd.motorUseTime", const.MOTOR_USE_TIME),
             LevelSensorEntity(client, self, "pd.oilVal", const.FUEL_LEVEL),
             MiscSensorEntity(client, self, "pd.errCode", const.ERROR_CODE),
+            MiscSensorEntity(client, self, "pd.ver", const.SOFTWARE_VERSION),
             WattsSensorEntity(client, self, "pd.totalPower", const.TOTAL_OUT_POWER),
             WattsSensorEntity(client, self, "pd.acPower", const.AC_OUT_POWER),
             WattsSensorEntity(client, self, "pd.dcPower", const.DC_OUT_POWER), 
             RemainSensorEntity(client, self, "pd.remainTime", const.REMAINING_TIME),
             VoltSensorEntity(client, self, "pd.dcVol", const.DC_OUT_VOLTAGE),
             VoltSensorEntity(client, self, "pd.acVol", const.AC_OUT_VOLT),
+            CelsiusSensorEntity(client, self, "pd.temp", const.TEMPERATURE),
             #AmpSensorEntity(client, self, "pd.dcCur", const.DC_OUT_),
             #AmpSensorEntity(client, self, "pd.acCur", const.AC_OUT_CURRENT),
         ]
@@ -31,10 +34,14 @@ class Generator_DualFuel(BaseDevice):
             EnabledEntity(client, self, "pd.sysMode", const.MOTOR_ECO,
                           lambda value: {"isMatter": 0,"moduleType": 2, "operateType": "modeCtrl", "params": {"mode": value}}),
                           
-                          ]
+        ]
 
     def numbers(self, client: EcoflowApiClient) -> list[BaseNumberEntity]:
-        return []
+        return [
+            ChargingPowerEntity(client, self, "pd.oilMaxOutPower","Max Oil Output Power", 1000, 1800,  
+                lambda value: {"isMatter": 0, "moduleType": 2, "operateType": "oilMaxOutPower", "params": {"oilMax": value}}),
+        
+        ]
 
     def selects(self, client: EcoflowApiClient) -> list[BaseSelectEntity]:
         return []
